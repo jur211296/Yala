cf779349fe50a999f3741b2df01c30573bed25bf
Jurgen Schmidt <jur211296@gmail.com>
2025-11-11 15:07:40 -0500

    Initial Commit

---
+++ FinariaTests/FinariaTests.swift
@@ -0,0 +1,17 @@
+//
+//  FinariaTests.swift
+//  FinariaTests
+//
+//  Created by Trabajo on 11/11/25.
+//
+
+import Testing
+@testable import Finaria
+
+struct FinariaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
