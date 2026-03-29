87f1e7f86d95afd5c7f8887b0285259a27e761a0
jur211296 <jur211296@gmail.com>
2026-03-29 09:58:17 -0500

    feat: extend free trial from 7 to 30 days

--- Yala/App/Services/StoreKitManager.swift
+++ Yala/App/Services/StoreKitManager.swift
@@ -55,9 +55,9 @@ final class StoreKitManager {
         return max(0, days)
     }
 
-    /// Whether trial is expiring soon (2 days or less)
+    /// Whether trial is expiring soon (5 days or less)
     var isTrialExpiringSoon: Bool {
-        isInTrial && trialDaysRemaining <= 2 && trialDaysRemaining > 0
+        isInTrial && trialDaysRemaining <= 5 && trialDaysRemaining > 0
     }
 
     // MARK: - Downgrade Detection
